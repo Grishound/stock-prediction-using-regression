88c65deeffe934511e6723b56e815809542ae075
Mayank Parashar <52569348+Grishound@users.noreply.github.com>
2024-02-06 12:00:47 +0530

    Update regression.py

--- regression.py
+++ regression.py
@@ -11,9 +11,8 @@ stock = 'AAPL'
 start_date = dt.date.today() - dt.timedelta(days=3650)  # 10 years of data
 end_date = dt.date.today()
 data = yf.download(stock, start_date, end_date)
-data = data.drop(columns=['Adj Close'])
 
-X = data.drop(['Close'], axis=1)
+X = data.drop(['Adj Close'], axis=1)
 y = data['Adj Close']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
 
@@ -24,6 +23,6 @@ print(f"The intercept for our model is: {intercept}")
 
 score = regression_model.score(X_test, y_test)
 print(f"The score for our model is: {score}")
-latest_data = data.tail(1).drop(['Close'], axis=1)
+latest_data = data.tail(1).drop(['Adj Close'], axis=1)
 next_day_price = regression_model.predict(latest_data)[0]
 print(f"The predicted price for the next trading day is: {next_day_price}")
